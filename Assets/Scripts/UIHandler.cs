5e8a344b0e16069467349933a9aded4f462c917e
Agustina Vinciguerra <agustina.vinciguerra@unraf.edu.ar>
2025-05-15 21:35:06 -0300

    Animations
    - Added animations to visualization
    - Pair names now appear on different sides

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class UIHandler : MonoBehaviour
 {
@@ -10,20 +11,41 @@ public class UIHandler : MonoBehaviour
         public System.Collections.Generic.List<string> parejas;
     }
 
-    [SerializeField] private TMP_Text parejasDisplayText;
+    [SerializeField] private TMP_Text playerOne;
+    [SerializeField] private TMP_Text playerTwo;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject versusGameObject;
+    [SerializeField] private GameObject waitingGameObject;
 
     public void OnMessageReceived(string message)
     {
+        StartCoroutine(ShowMessageWithExit(message));
+    }
+
+    private IEnumerator ShowMessageWithExit(string message)
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("exit");
+        }
+        yield return new WaitForSeconds(1f);
+
+        versusGameObject.SetActive(true);
+        waitingGameObject.SetActive(false);
+
         Debug.Log("Mensaje recibido en UIHandler: " + message);
-        
+
         Wrapper received = JsonUtility.FromJson<Wrapper>(message);
         if (received != null && received.parejas != null && received.parejas.Count > 0)
         {
-            parejasDisplayText.text = received.parejas[0]; // Solo la primera pareja
+            string[] jugadores = received.parejas[0].Split(new string[] { " vs " }, System.StringSplitOptions.None);
+            playerOne.text = jugadores.Length > 0 ? jugadores[0].ToUpper() : "";
+            playerTwo.text = jugadores.Length > 1 ? jugadores[1].ToUpper() : "";
         }
         else
         {
-            parejasDisplayText.text = "No hay parejas.";
+            playerOne.text = "";
+            playerTwo.text = "";
         }
     }
 }
